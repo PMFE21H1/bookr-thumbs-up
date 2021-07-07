b5d7607124bbee0cf69e51499ac35f4c8bc3da63
LauraS <71083139+LauraS-i@users.noreply.github.com>
2021-07-07 19:10:08 +0200

    createResource function added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { createResource, Resource } from "./resources/resources";
+
+let resource = new Resource("Room 303");
+let promise = createResource(resource);
+promise.then(created => console.log(created));
+
+
 function App() {
   return (
     <div>
