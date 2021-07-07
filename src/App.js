86ee195b851cf4a7fe242a569aeb53fca90bad6b
LauraS <71083139+LauraS-i@users.noreply.github.com>
2021-07-07 20:53:12 +0200

    delete: test files from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-
-import { createResource, Resource } from "./resources/resources";
-
-let resource = new Resource("Room 303");
-let promise = createResource(resource);
-promise.then(created => console.log(created));
-
-
 function App() {
   return (
       <div>
