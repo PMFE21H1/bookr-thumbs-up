dfb6e9ae0eb1e528b17b18e095f5e11f07874745
patrik <you@example.com>
2021-07-07 13:09:22 +0200

    fixing empty string problem

--- src/resources/resources.js
+++ src/resources/resources.js
@@ -1,6 +1,6 @@
 export class Resource {
     constructor(name, id) {
-        if(name == null) throw new Error('Name can not be empty')
+        if(!name) throw new Error('Name can not be empty')
         this.name = name;
         this.id = id;
     }
