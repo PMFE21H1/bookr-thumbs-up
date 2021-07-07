6c2943ec3a0f45e4550d1ab866d30fc8f48d1496
udvardiakos <82174805+udvardiakos@users.noreply.github.com>
2021-07-07 21:24:45 +0200

    updateresourcepage half done

--- src/resources/UpdateResourcePage.js
+++ src/resources/UpdateResourcePage.js
@@ -3,11 +3,15 @@ import React from "react";
 export class UpdateResourcePage extends React.Component{
     constructor(props) {
         super(props);
+
+
+
     }
     render() {
        return(
                <>
-
+                    <input type="text" onChange={} value={}/>
+                    <Link to="/admin/resource">Cancel</Link>
                </>
            )
 
