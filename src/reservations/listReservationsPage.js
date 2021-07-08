b850ec7281736f4323d6aead471e7730ce78cdc2
patrik <you@example.com>
2021-07-08 22:32:21 +0200

    create list-reservations-page

--- src/reservations/listReservationsPage.js
+++ src/reservations/listReservationsPage.js
@@ -1,17 +1,47 @@
 import React, { Route, Link } from 'react'
+import { listReservations } from './reservations'
 export default class ListReservationsPage extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            reservations: null
+        }
+    }
+    getReservations = () => {
+        listReservations().then(reservations => this.setState({reservations: reservations}))
+    }
+    componentDidMount() {
+        this.getReservations()
     }
     render(){
         return(
-          < >
-            <Route path='/admin/reservations'/>
-            <p>{listReservations}</p>
-            <button><Link to={"/admin/reservations/:reservationID/delete/"}>Delete</Link></button>
-            <button><Link to={"/admin/reservations/:reservationID/edit/" + this.props.reservations.id}>Edit</Link></button>
+          <>
+            <Route path='/admin/reservations'>
+                <button>Add reservation</button>
+            <table>
+                <tr>
+                    <th>Reservations name</th><th>Reservations id</th>
+                </tr>
+                {
+                    this.state.reservations?
+                    this.state.reservations.map(reservation =>{
+                        return(
+                            <tr>
+                                <td>{reservation.name}</td>
+                                <td>{reservation.id}</td>
+                                <td><Link to={"/admin/reservations/:reservationID/delete/"}>Delete</Link></td>
+                                <td><Link to={"/admin/reservations/:reservationID/edit/"}>Edit</Link></td>
+                            </tr>
+                        )
+                    })
+                    :
+                    <li></li>
+                }
+            </table>
+            
+            
+            </Route>
             </>
         )
     }
 }
-// nem vagyok meg biztos benne, hogy melyik verzio fog nekunk kelleni(a url-es vagy a props alapu id kereses)
