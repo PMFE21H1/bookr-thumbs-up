e60198e004cf9d7fdf66bd145d75996364a665a5
Kristof A. Szabo <82156829+szabkriss@users.noreply.github.com>
2021-07-09 16:30:36 +0200

    wip: added reservations folder and create reservation page

---
+++ src/reservations/CreateReservationPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class CreateReservationPage extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            resource: "",
+            name:"",
+            date:"",
+            time:""
+        }
+    }
+
+
+    render() {
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
