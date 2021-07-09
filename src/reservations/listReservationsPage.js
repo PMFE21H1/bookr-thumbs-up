0092da65f06b636b49afa16a1dbf950da6e832f6
patrik <you@example.com>
2021-07-09 11:47:29 +0200

    code shorcut

--- src/reservations/listReservationsPage.js
+++ src/reservations/listReservationsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from 'react-router-dom'
+import { Route, Link } from "react-router-dom";
 import { listReservations } from "./reservations";
 
 export default class ListReservationsPage extends React.Component {
@@ -9,38 +9,37 @@ export default class ListReservationsPage extends React.Component {
       reservations: null,
     };
   }
-  getReservations = () => {
+
+  componentDidMount() {
     listReservations().then((reservations) =>
       this.setState({ reservations: reservations })
     );
-  };
-  componentDidMount() {
-    this.getReservations();
   }
+
   render() {
     return (
       <>
         {/* <Route path="/admin/reservations"> */}
-          <button>Add reservation</button>
-          <table>
-            <tr>
-              <th>Reservations customer</th>
-              <th>Reservations id</th>
-              <th>Reservations time</th>
-              <th>Reservations resource-id</th>
-            </tr>
-            {this.state.reservations ? (
-              this.state.reservations.map((reservation) => {
-                return (
-                  <tr>
-                    <td>{reservation.customer}</td>
-                    <td>{reservation.id}</td>
-                    <td>{reservation.slot}</td>
-                    <td>{reservation.resource}</td>
-                    <td>
-                        <button>Edit</button>
-                        <button>Delete</button>
-                      {/* <Link to={"/admin/reservations/:reservationID/delete/"}>
+        <button>Add reservation</button>
+        <table>
+          <tr>
+            <th>Reservations customer</th>
+            <th>Reservations id</th>
+            <th>Reservations time</th>
+            <th>Reservations resource-id</th>
+          </tr>
+          {this.state.reservations ? (
+            this.state.reservations.map((reservation) => {
+              return (
+                <tr>
+                  <td>{reservation.customer}</td>
+                  <td>{reservation.id}</td>
+                  <td>{reservation.slot}</td>
+                  <td>{reservation.resource}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                    {/* <Link to={"/admin/reservations/:reservationID/delete/"}>
                         Delete
                       </Link>
                     </td>
@@ -48,14 +47,14 @@ export default class ListReservationsPage extends React.Component {
                       <Link to={"/admin/reservations/:reservationID/edit/"}>
                         Edit
                       </Link> */}
-                    </td>
-                  </tr>
-                );
-              })
-            ) : (
-              <p></p>
-            )}
-          </table>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <p></p>
+          )}
+        </table>
         {/* </Route> */}
       </>
     );
