18b36d051f58be670f25041a7a6d3a9503fafb7e
Schneider Sara <schneiders92@users.noreply.github.com>
2021-07-09 16:36:53 +0200

    deleting unrelevant code from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { createResource, Resource, deleteResource } from "./resources/resources";
 
-deleteResource({id:"-Me4-u_eL46RZwMgEiCJ", name: "Room 303"})
+
+
 
 function App() {
   return (
