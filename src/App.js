da0b039241183f93109077b2d222fcd8bc2af153
Kristof A. Szabo <82156829+szabkriss@users.noreply.github.com>
2021-07-09 14:01:24 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+
+
 function App() {
   return (
-      <div>
-      </div>
+      <>
+      <Router>
+        <Switch>
+
+          <Route path="/login">
+              <LoginPage/>
+          </Route>
+
+                {/* RESOURCES */}
+
+          <Route path="/admin/resource/:id/edit">
+              <EditResourcePage/>
+          </Route>
+
+          <Route path="/admin/resource/:id/delete">
+              <DeleteResourcePage/>
+          </Route>
+
+          <Route path="/admin/resource/create">
+              <CreateResourcePage/>
+          </Route>
+
+          <Route path="/admin/resource">
+              <ListResourcesPage/>
+          </Route>
+
+                {/* RESERVATIONS */}
+
+          <Route path="/admin/reservations/:id/edit">
+              <EditReservationPage/>
+          </Route>
+
+          <Route path="/admin/reservations/:id/delete">
+              <DeleteReservationPage/>
+          </Route>
+
+          <Route path="/admin/reservations/create">
+              <CreateReservationPage/>
+          </Route>
+
+          <Route path="/admin/reservations">
+              <ListReservationsPage/>
+          </Route>
+
+                {/* HOME PAGE */}
+
+          <Route path="/">
+              <HomePage/>
+          </Route>
+
+        </Switch>
+      </Router>
+
+        
+
+      </>
   );
 }
 
