a177cd5a02ae3a7c87b86ac58f02d1699f7e6cec
udvardiakos <82174805+udvardiakos@users.noreply.github.com>
2021-07-10 15:35:35 +0200

    deletereservation function moved to reservations.js module

--- src/reservations/reservations.js
+++ src/reservations/reservations.js
@@ -75,3 +75,17 @@ export class Reservation{
         this.slot = slot
     }
 }
+
+export function deleteReservation(id) {
+    if (!id) throw new Error("Id can not be empty");
+    return fetch(
+        `https://bookr-thumbs-up-default-rtdb.europe-west1.firebasedatabase.app/reservations/${id}.json`,
+        {
+            method: "DELETE",
+        }
+    )
+        .then((response) => response.json())
+        .then((deletedReservation) =>
+            console.log(deletedReservation)
+        );
+}
