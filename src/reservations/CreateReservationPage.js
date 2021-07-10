b6f0fe49661ac61f33a63a71da08b229fd508188
Kristof A. Szabo <82156829+szabkriss@users.noreply.github.com>
2021-07-10 11:46:57 +0200

    added some stuff

--- src/reservations/CreateReservationPage.js
+++ src/reservations/CreateReservationPage.js
@@ -7,15 +7,56 @@ export default class CreateReservationPage extends Component {
             resource: "",
             name:"",
             date:"",
-            time:""
+            time:"",
+            newReservation:{
+
+            }
+            
         }
     }
 
+    changeName = (e) => {
+        this.setState({name: e.target.value})
+    }
 
+    changeDate = (e) => {
+        this.setState({date: e.target.value})
+    }
+
+    changeTime = (e) => {
+        this.setState({time: e.target.value})
+    }
+
+    changeResource = (e) => {
+        this.setState({resource: e.target.value})
+    }
+ 
     render() {
         return (
             <>
-                
+                <div>
+                    <label>Resource:</label>
+                    <select onChange={e =>this.changeResource(e)} value={this.state.resource}></select>
+                </div>
+
+                <div>
+                    <label>Name:</label>
+                    <input onChange={(e)=>this.changeName(e)} value={this.state.name}></input>
+                </div>
+
+                <div>
+                    <label>Date:</label>
+                    <input type="date" onChange={(e)=>this.changeDate(e)} value={this.state.name}></input>
+                </div>
+
+                <div>
+                    <label>Time:</label>
+                    <input type="time" onChange={(e)=>this.changeTime(e)} value={this.state.name}></input>
+                </div>
+
+                <button>Create</button>
+                <button>Cancel</button>
+
             </>
         )
     }
