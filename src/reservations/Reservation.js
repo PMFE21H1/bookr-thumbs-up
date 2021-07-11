f862673b51b293929fc0cd7281b99d938df7b608
udvardiakos <82174805+udvardiakos@users.noreply.github.com>
2021-07-11 21:40:26 +0200

    fix reservation class if

--- src/reservations/Reservation.js
+++ src/reservations/Reservation.js
@@ -1,15 +1,14 @@
-let regexpDate = "^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2})$";
-console.log(("2017-01-01T20:00").match(regexpDate));
 
-class Reservation{
+
+export class Reservation{
     constructor(customer,resource,slot,id) {
         if(!customer || typeof customer != "string"){
             throw new Error("Customer declaration invalid")
         }
-        if(!resource || typeof resource != "string"){
+        if(!resource ){
             throw new Error("Bad resource declaration!")
         }
-        if(!slot || !slot.match(regexpDate)){
+        if(!slot || !slot.match("^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2})$")){
             throw new Error("Bad slot declaration!")
         }
         this.id = ""
