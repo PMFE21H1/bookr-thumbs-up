e1419874dc930f131ecb408b6dad552787402d7b
Schneider Sara <schneiders92@users.noreply.github.com>
2021-07-12 08:41:46 +0200

    route added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CreateResourcePage from "./resources/CreateResourcePage";
 import ListReservationsPage from "./reservations/listReservationsPage";
 import ListResourcesAdminPage from "./resources/ListResourcesAdminPage";
 import DeleteReservationPage from "./reservations/DeleteReservationPage";
+import DeleteResourcePage from "./resources/DeleteResourcePage";
 
 function App() {
     return (
@@ -17,8 +18,8 @@ function App() {
                 {/*<Route path="/admin/resource/:resourceID/edit" render={(props)=>(<UpdateResourceAdmin {...props}/>)}>*/}
                 {/*</Route>*/}
                 {/*<Route path="/admin/reservations/create" exact><ReservationPage/></Route>*/}
-                <Route path="/admin/reservations/:reservationID/delete" render={(props)=>(<DeleteReservationPage {...props}/>)}>
-                </Route>
+                <Route path="/admin/resources/:resourceID/delete" render={(props)=>(<DeleteResourcePage {...props}/>)}></Route>
+                <Route path="/admin/reservations/:reservationID/delete" render={(props)=>(<DeleteReservationPage {...props}/>)}></Route>
                 <Route path="/admin/reservations" exact><ListReservationsPage/></Route>
             </Switch>
             <Link to="/admin/resources">Resources</Link>
