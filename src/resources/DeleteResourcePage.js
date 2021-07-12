4f1372a1b80740d6e4b56968afd5c6d53cad127a
Schneider Sara <schneiders92@users.noreply.github.com>
2021-07-12 09:26:41 +0200

    theoretically ready, test needed

--- src/resources/DeleteResourcePage.js
+++ src/resources/DeleteResourcePage.js
@@ -1,19 +1,38 @@
 
 import React from "react"
+import {listReservations} from "../reservations/reservations";
 
 export default class DeleteResourcePage extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-
+            resourceId:this.props.match.params.resourceID,
+            reservations:[],
         }
     }
+    componentDidMount() {
+
+        //lekérem az összes reservation adatát, hogy ki tudjam írni a relevánsakat
+        listReservations().then(reservations=>this.setState({
+            reservations:reservations
+        }))
+
+    }
+
     render(){
         return(
             <div>
-                <p>Are you sure you want to delete the following resource?</p>
+                <p>Are you sure you want to delete the resource along with the following reservations?</p>
+                {this.state.reservations.map(reservation=> {
+                    if(reservation.resource===this.state.resourceId){
+                        return(<div><span>{reservation.name}, {reservation.slot}</span></div>)
+                    }
+                })}
+                <button onClick={()=>deleteResource(this.state.resourceId)}>Delete</button>
+                {/*<Link to={"admin/resources"}></Link>*/}
 
             </div>
         )
     }
 }
+
