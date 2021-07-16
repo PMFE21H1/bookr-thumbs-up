e91c029c7ce3af592d5d51ee6a29bb3ca7f3eab3
Kristof A. Szabo <82156829+szabkriss@users.noreply.github.com>
2021-07-16 10:54:37 +0200

    formatted code

--- src/authentication/authentication.js
+++ src/authentication/authentication.js
@@ -17,21 +17,21 @@ export class User extends Component {
 
 export function saveAccountToDatabase(name, email, admin, uid) {
 
-                            return fetch("https://bookr-thumbs-up-default-rtdb.europe-west1.firebasedatabase.app/users.json", {
-                                body: JSON.stringify({name: name, email: email, admin: admin, uid: uid}),
-                                method: "POST"
-                            }).then(response => {
-                                //response-ból kiolvassuk a státuszkódot, és az alapján adunk vissza alert message-et
-                                if (response.status === 200) {
-                                    alert("Successful user creation!")
-                                } else {
-                                    alert("Unsuccessful!")
-                                }
+return fetch("https://bookr-thumbs-up-default-rtdb.europe-west1.firebasedatabase.app/users.json", {
+    body: JSON.stringify({name: name, email: email, admin: admin, uid: uid}),
+    method: "POST"
+}).then(response => {
+    //response-ból kiolvassuk a státuszkódot, és az alapján adunk vissza alert message-et
+    if (response.status === 200) {
+        alert("Successful user creation!")
+    } else {
+        alert("Unsuccessful!")
+    }
 
-                                return response.json()
-                            })
-                                //itt jön létre az új reservation a Reservation class használatával
-                                .then(data => {
-                                        console.log(data)
-                                })
-        }
+    return response.json()
+})
+    //itt jön létre az új reservation a Reservation class használatával
+    .then(data => {
+            console.log(data)
+    })
+}
