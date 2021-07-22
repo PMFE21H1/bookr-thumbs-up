25ccdf3a7f5762afbedba612592c799426ae6e1e
patrik <you@example.com>
2021-07-22 17:39:36 +0200

    bootstrap beginnings

--- src/authentication/Header.js
+++ src/authentication/Header.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import { AuthContext, UsersDatabaseContext } from "../context/context";
 import { Link } from "react-router-dom";
 import { getUserByUid } from "./authentication";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
 
 export default class Header extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
@@ -18,29 +18,68 @@ export default class Header extends Component {
             if (!user) {
               return (
                 <>
-                  <Link to="/login">Log In</Link>
-                  <Link to="/registration">Registration</Link>
-                  <Link to="/resources">Public Recources</Link>
+                  <Nav className="justify-content-center" activeKey="/home">
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/login">
+                        Log In
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/registration">
+                        Registration
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/resources">
+                        Public Recources
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
                 </>
               );
             } else {
-                  return user.admin ? 
-                    
-                    <>
-                      <Link to="/admin/resources">Resources</Link>
-                      <Link to="/admin/reservations">Reservations</Link>
-                      <p>Admin</p>
-                    </>
-
-                    :
-
-                    <>
-                      <Link to="/my-reservations">Reservations</Link>
-                      <Link to="/resources">Public Recources</Link>
-                      <p>User</p>
-                    </>
-
-                    }
+              return user.admin ? (
+                <>
+                  <Nav className="justify-content-center" activeKey="/home">
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/admin/resources">
+                        Resources
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/admin/reservations">
+                        Reservations
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="disabled" disabled>
+                        Admin
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </>
+              ) : (
+                <>
+                  <Nav className="justify-content-center" activeKey="/home">
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/my-reservations">
+                        Reservations
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link as={Link} to="/resources">
+                        Public Recources
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="disabled" disabled>
+                        User
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </>
+              );
+            }
           }}
         </AuthContext.Consumer>
       </div>
--- src/reservations/listReservationsPage.js
+++ src/reservations/listReservationsPage.js
@@ -1,132 +1,160 @@
 import React from "react";
-import {Route, Link} from "react-router-dom";
-import {AuthContext, UsersDatabaseContext} from "../context/context"
-import {listResources} from "../resources/resources";
-import {listReservations, confirmReservation} from "./reservations";
+import { Route, Link } from "react-router-dom";
+import { AuthContext, UsersDatabaseContext } from "../context/context";
+import { listResources } from "../resources/resources";
+import { listReservations, confirmReservation } from "./reservations";
+import Table from "react-bootstrap/Table";
 
 export default class ListReservationsPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            resources: [],
-            confirmed: [],
-            pending:[]
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      resources: [],
+      confirmed: [],
+      pending: [],
+    };
+  }
 
-    componentDidMount() {
-        listResources().then((resources) => {
-                this.setState({resources: resources})
-            }
-        )
-        let confirmed = [];
-        let pending = [];
-        listReservations().then((reservations) => {
-            reservations.forEach(reservation => reservation.status === "confirmed" ? confirmed.push(reservation) : pending.push(reservation))
-        })
-            .then(() => this.setState({confirmed: confirmed, pending: pending}));
-
-    }
-
-    render() {
-
-        return (
-
-            <UsersDatabaseContext>
-                {users => {
-                    return <>
-                <Link to={"/admin/reservations/create"}>Add reservation</Link>
-                <Link to={"/admin/config"}>Config</Link>
-
-                <h2>Confirmed Reservations</h2>
-                <table>
-                    <tr>
-                        <th>Reservations customer</th>
-                        <th>Reservations id</th>
-                        <th>Reservations time</th>
-                        <th>Resource</th>
-                    </tr>
-                    {this.state.confirmed.map(reservation => {
-                        //itt kell majd a userDatabaseConsumer
-                                return (
-                                    <tr>
-                                        <td>{users.map(user => {if(reservation.customerUid === user.uid){ return user.name }} )}</td>
-                                        <td>{reservation.id}</td>
-                                        <td>{reservation.slot}</td>
-                                        <td>{this.state.resources.map(resource => reservation.resource == resource.id ? resource.name : "")}</td>
-                                        <td>
-                                            <Link
-                                                to={`/admin/reservations/${reservation.id}/delete`}>
-                                                DELETE
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            <Link
-                                                to={`/admin/reservations/${reservation.id}/edit`}>
-                                                EDIT
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            <Link
-                                                to={`/admin/reservations/${reservation.id}`}>
-                                                DETAILS
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                );
-
-                            })}
-                </table>
-
-
-                <h2>Pending Reservations</h2>
-                <table>
-                    <tr>
-                        <th>Reservations customer</th>
-                        <th>Reservations id</th>
-                        <th>Reservations time</th>
-                        <th>Resource</th>
-                    </tr>
-                    {
-                            this.state.pending.map(reservation => {
-                                return (
-                                    <tr>
-                                        <td>{reservation.customerUid}</td>
-                                        <td>{reservation.id}</td>
-                                        <td>{reservation.slot}</td>
-                                        <td>{this.state.resources.map(resource => reservation.resource == resource.id ? resource.name : "")}</td>
-                                        <td>
-                                            <Link
-                                                to={`/admin/reservations/${reservation.id}/delete`}>
-                                                DELETE
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            <Link
-                                                to={`/admin/reservations/${reservation.id}/edit`}>
-                                                EDIT
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            <Link
-                                                to={`/admin/reservations/${reservation.id}`}>
-                                                DETAILS
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            <button onClick={()=>confirmReservation(reservation.id)}>
-                                                CONFIRM
-                                            </button>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-
-
-                </table>
-        </>
-        }}
-        </UsersDatabaseContext>
+  componentDidMount() {
+    listResources().then((resources) => {
+      this.setState({ resources: resources });
+    });
+    let confirmed = [];
+    let pending = [];
+    listReservations()
+      .then((reservations) => {
+        reservations.forEach((reservation) =>
+          reservation.status === "confirmed"
+            ? confirmed.push(reservation)
+            : pending.push(reservation)
         );
-    }
+      })
+      .then(() => this.setState({ confirmed: confirmed, pending: pending }));
+  }
+
+  render() {
+    return (
+      <UsersDatabaseContext>
+        {(users) => {
+          return (
+            <>
+              <Link to={"/admin/reservations/create"}>Add reservation</Link>
+              <Link to={"/admin/config"}>Config</Link>
+
+              <h2>Confirmed Reservations</h2>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Reservations customer</th>
+                    <th>Reservations id</th>
+                    <th>Reservations time</th>
+                    <th>Resource</th>
+                  </tr>
+                </thead>
+
+                {this.state.confirmed.map((reservation) => {
+                  //itt kell majd a userDatabaseConsumer
+                  return (
+                    <tbody>
+                      <tr>
+                        <td>
+                          {users.map((user) => {
+                            if (reservation.customerUid === user.uid) {
+                              return user.name;
+                            }
+                          })}
+                        </td>
+                        <td>{reservation.id}</td>
+                        <td>{reservation.slot}</td>
+                        <td>
+                          {this.state.resources.map((resource) =>
+                            reservation.resource == resource.id
+                              ? resource.name
+                              : ""
+                          )}
+                        </td>
+                        <td>
+                          <Link
+                            to={`/admin/reservations/${reservation.id}/delete`}
+                          >
+                            DELETE
+                          </Link>
+                        </td>
+                        <td>
+                          <Link
+                            to={`/admin/reservations/${reservation.id}/edit`}
+                          >
+                            EDIT
+                          </Link>
+                        </td>
+                        <td>
+                          <Link to={`/admin/reservations/${reservation.id}`}>
+                            DETAILS
+                          </Link>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+              </Table>
+
+              <h2>Pending Reservations</h2>
+              <Table>
+                <tbody>
+                  <tr>
+                    <th>Reservations customer</th>
+                    <th>Reservations id</th>
+                    <th>Reservations time</th>
+                    <th>Resource</th>
+                  </tr>
+                  {this.state.pending.map((reservation) => {
+                    return (
+                      <tr>
+                        <td>{reservation.customerUid}</td>
+                        <td>{reservation.id}</td>
+                        <td>{reservation.slot}</td>
+                        <td>
+                          {this.state.resources.map((resource) =>
+                            reservation.resource == resource.id
+                              ? resource.name
+                              : ""
+                          )}
+                        </td>
+                        <td>
+                          <Link
+                            to={`/admin/reservations/${reservation.id}/delete`}
+                          >
+                            DELETE
+                          </Link>
+                        </td>
+                        <td>
+                          <Link
+                            to={`/admin/reservations/${reservation.id}/edit`}
+                          >
+                            EDIT
+                          </Link>
+                        </td>
+                        <td>
+                          <Link to={`/admin/reservations/${reservation.id}`}>
+                            DETAILS
+                          </Link>
+                        </td>
+                        <td>
+                          <button
+                            onClick={() => confirmReservation(reservation.id)}
+                          >
+                            CONFIRM
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </>
+          );
+        }}
+      </UsersDatabaseContext>
+    );
+  }
 }
