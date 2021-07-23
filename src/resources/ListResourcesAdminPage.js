40c95506082a67a1c85f0ccb621dcea84d49c3a4
patrik <you@example.com>
2021-07-23 12:27:19 +0200

    still wip

--- src/resources/ListResourcesAdminPage.js
+++ src/resources/ListResourcesAdminPage.js
@@ -1,59 +1,128 @@
-import { listResources } from './resources'
-import { Link } from 'react-router-dom'
-import React from 'react'
+// import { listResources } from './resources'
+// import { Link } from 'react-router-dom'
+// import React from 'react'
 
-class listResourceAdminPage extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            resources: null
-        }
-    }
+// class listResourceAdminPage extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             resources: null
+//         }
+//     }
 
-    getResources = () => {
-        listResources().then( resources => this.setState({resources: resources}))
-    }
+//     getResources = () => {
+//         listResources().then( resources => this.setState({resources: resources}))
+//     }
 
-    componentDidMount(){
-        this.getResources()
-    }
+//     componentDidMount(){
+//         this.getResources()
+//     }
 
-render(){
-    return(
-        <>
-            <Link to="/admin/resources/create">Add resource</Link>
-    
-        <table>
+// render(){
+//     return(
+//         <>
+//             <Link to="/admin/resources/create">Add resource</Link>
+
+//         <table>
+//             <tr>
+//                 <th>Resource Name</th><th>Resource ID</th><th></th>
+//             </tr>
+//                 {
+//                 this.state.resources ?
+//                 this.state.resources.map(resource => {
+//                     return (
+//                     <tr>
+//                         <td>{resource.name}</td>
+//                         <td>{resource.id}</td>
+//                         <td>
+
+//                         </td>
+
+//                          <td><Link
+//                          to={`/admin/resource/${resource.id}/edit`}>EDIT</Link></td>
+//                         <td><Link to={`/admin/resource/${resource.id}/delete`}>DELETE</Link></td>
+//                     </tr>)
+//                 })
+//             :
+//                 <p></p>
+//             }
+
+//         </table>
+
+//         </>
+
+//         )
+//     }
+// }
+
+// export default listResourceAdminPage
+
+import { listResources } from "./resources";
+import { Link } from "react-router-dom";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
+
+class listResourceAdminPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resources: null,
+    };
+  }
+
+  getResources = () => {
+    listResources().then((resources) =>
+      this.setState({ resources: resources })
+    );
+  };
+
+  componentDidMount() {
+    this.getResources();
+  }
+
+  render() {
+    return (
+      <>
+        <Link to="/admin/resources/create">Add resource</Link>
+
+        <Table striped bordered hover>
+          <thead>
             <tr>
-                <th>Resource Name</th><th>Resource ID</th><th></th>
+              <th>Resource Name</th>
+              <th>Resource ID</th>
             </tr>
-                {
-                this.state.resources ?
-                this.state.resources.map(resource => {
-                    return (
-                    <tr>
-                        <td>{resource.name}</td>
-                        <td>{resource.id}</td>
-                        <td>
+          </thead>
 
+          {this.state.resources ? (
+            this.state.resources.map((resource) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td>{resource.name}</td>
+                    <td>{resource.id}</td>
 
-                        </td>
-
-                         <td><Link
-                         to={`/admin/resource/${resource.id}/edit`}>EDIT</Link></td>
-                        <td><Link to={`/admin/resource/${resource.id}/delete`}>DELETE</Link></td>
-                    </tr>)
-                })
-            :
-                <p></p>
-            }
-            
-        </table>
-
-        </>
-        
-        )
-    }
+                    <td>
+                      <Link to={`/admin/config/resource/${resource.id}/edit`}>
+                        EDIT
+                      </Link>
+                    </td>
+                    <td>
+                      <Link to={`/admin/config/resource/${resource.id}/delete`}>
+                        DELETE
+                      </Link>
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })
+          ) : (
+            <p></p>
+          )}
+        </Table>
+      </>
+    );
+  }
 }
 
-export default listResourceAdminPage
+export default listResourceAdminPage;
--- src/resources/UpdateResourcePage.js
+++ src/resources/UpdateResourcePage.js
@@ -1,7 +1,90 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { updateResource } from "./resources";
+
+// export class UpdateResourcePage extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       newResourceName: "",
+//       resourceID: this.props.match.params.resourceID,
+//       oldResourceName: "",
+//       // felvettuk a descriptionokat a statbe, hasonlo mintara mint a resourcok amik mar benne voltak
+//       oldDescription: "",
+//       newDescription: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch(
+//       `https://bookr-thumbs-up-default-rtdb.europe-west1.firebasedatabase.app/resources/${this.state.resourceID}.json`
+//     )
+//       .then((response) => response.json())
+//       .then((resource) =>
+//         this.setState({
+//           oldResourceName: resource.name,
+//           //a fetchelt adatokbol setStateltuk a mar meglevo descriptiont
+//           oldDescription: resource.description,
+//         })
+//       );
+//   }
+
+//   updateResourceName = (e) => {
+//     this.setState({
+//       newResourceName: e.target.value,
+//     });
+//   };
+//   //az uj description kezelesere csinaltunk egy metodust
+//   updateDescription = (e) => {
+//     this.setState({
+//       newDescription: e.target.value,
+//     });
+//   };
+//   //kiszerveztuk az onClickben megirt korabbi feladatokat
+//   handleApply = () => {
+//     try {
+//       updateResource(this.state.resourceID, {
+//         name: this.state.newResourceName,
+//         description: this.state.newDescription,
+//       }).then(() => this.props.history.push("/admin/resources"));
+//     } catch (e) {
+//       alert(e.message);
+//     }
+//   };
+//   render() {
+//     return (
+//       <>
+//         {this.state.oldResourceName ? (
+//           <div>
+//             <input
+//               type="text"
+//               placeholder={this.state.oldResourceName}
+//               onChange={this.updateResourceName}
+//               value={this.state.newResourceName}
+//             />
+//             {/* a descripiton hossza miatt textareat hasznalunk a sima input helyett */}
+//             <textarea
+//               placeholder={this.state.oldDescription}
+//               onChange={this.updateDescription}
+//               value={this.state.newDescription}
+//             />
+//             <button onClick={this.handleApply}>Apply</button>
+//             <Link to="/admin/resources">Cancel</Link>
+//           </div>
+//         ) : (
+//           <p></p>
+//         )}
+//       </>
+//     );
+//   }
+// }
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { updateResource } from "./resources";
-
+import InputGroup from "react-bootstrap/InputGroup";
+import { FormControl, Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 export class UpdateResourcePage extends React.Component {
   constructor(props) {
     super(props);
@@ -56,20 +139,43 @@ export class UpdateResourcePage extends React.Component {
       <>
         {this.state.oldResourceName ? (
           <div>
-            <input
-              type="text"
-              placeholder={this.state.oldResourceName}
-              onChange={this.updateResourceName}
-              value={this.state.newResourceName}
-            />
+            <InputGroup size="sm" className="mb-3">
+              <InputGroup.Text id="inputGroup-sizing-sm">
+                Resource Name
+              </InputGroup.Text>
+              <FormControl
+                type="text"
+                onChange={this.updateResourceName}
+                value={this.state.newResourceName}
+                placeholder={this.state.oldResourceName}
+                aria-label="Small"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+
             {/* a descripiton hossza miatt textareat hasznalunk a sima input helyett */}
-            <textarea
-              placeholder={this.state.oldDescription}
-              onChange={this.updateDescription}
-              value={this.state.newDescription}
-            />
-            <button onClick={this.handleApply}>Apply</button>
-            <Link to="/admin/resources">Cancel</Link>
+
+            <InputGroup
+              controlId="floatingTextarea"
+              label={this.state.oldDescription}
+              className="mb-3"
+            >
+              <InputGroup.Text id="inputGroup-sizing-sm">
+                Resource Description
+              </InputGroup.Text>
+              <Form.Control
+                placeholder={this.state.oldDescription}
+                onChange={this.updateDescription}
+                value={this.state.newDescription}
+                as="textarea"
+              />
+            </InputGroup>
+
+            <Button onClick={this.handleApply}>Apply</Button>
+
+            <Link to="/admin/config/resources">
+              <Button variant="danger">Cancel</Button>
+            </Link>
           </div>
         ) : (
           <p></p>
