2be777607f67e7824595812aeb9e97e8b7432fe3
Schneider Sara <schneiders92@users.noreply.github.com>
2021-07-27 21:26:08 +0200

    wip

--- src/resources/PublicResourcesPage.js
+++ src/resources/PublicResourcesPage.js
@@ -1,59 +1,55 @@
 import React from "react";
-import { listResources } from "./resources";
-import { Link } from "react-router-dom";
+import {listResources} from "./resources";
+import {Link} from "react-router-dom";
 import {
-  Table,
-  Col,
-  Container,
-  Row,
-  Form,
-  Button,
-  Nav,
-  Card,
+    Table,
+    Col,
+    Container,
+    Row,
+    Form,
+    Button,
+    Nav,
+    Card,
 } from "react-bootstrap";
 import "./publicResource.css"
 
 
 export default class PublicResourcesPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      resources: [],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            resources: [],
+        };
+    }
 
-  componentDidMount() {
-    listResources().then((resources) =>
-      this.setState({
-        resources: resources,
-      })
-    );
-  }
+    componentDidMount() {
+        listResources().then((resources) =>
+            this.setState({
+                resources: resources,
+            })
+        );
+    }
 
-  render() {
-    return (
-      <div className="public-res-cont" style={{width:"100%", marginLeft: 'auto', marginRight:'auto'}}>
+    render() {
+        return (
+            <>
+                <div className="public-res-container">
+                    {this.state.resources.map((resource) => {
+                        return <div className="pub-res"
+                                    style={{backgroundImage:resource.imgUrl?`url("${resource.imgUrl}")`: "url('defaultBarber.jpg')"}}
+                        onMouseEnter={}>
+                            <div className="public-res-inner-container">
+                            <h5>{resource.name}</h5>
+                            <p>{resource.description}</p>
+                            <Link to={`/resources/${resource.id}/request-reservation`}>
+                                Request Reservation
+                            </Link>
+                            </div>
 
-
-            {this.state.resources.map((resource) => (
-
-                  <Card style={{textAlign: 'center', width: '18rem', padding: '0px 0px 10px 0px' }}>
-                    
-                    <Card.Img variant="top" src={resource.imgUrl ? resource.imgUrl : 'defaultBarber.jpg'} />
-                    <Card.Title className="res-name">{resource.name}</Card.Title>{" "}
-                    <Card.Text> {resource.description} </Card.Text>{" "}
-                    <Card.Link>
-                    <Link to={`/resources/${resource.id}/request-reservation`}>
-                      {" "}
-                      Request Reservation{" "}
-                    </Link>{" "}
-                    </Card.Link>
-                  </Card>
-
-            ))}
-
-
-      </div>
-    );
-  }
+                        </div>
+                    })}
+                </div>
+            </>
+        );
+    }
 }
