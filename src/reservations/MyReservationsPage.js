6b783677a00a400a3a14c230970d87c31e03cf2b
Kristof A. Szabo <82156829+szabkriss@users.noreply.github.com>
2021-07-28 16:40:46 +0200

    my reservation page made nice

--- src/reservations/MyReservationsPage.js
+++ src/reservations/MyReservationsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AuthContext, UsersDatabaseContext } from "../context/context";
+import { AuthContext, TaxonomyContext, UsersDatabaseContext } from "../context/context";
 import { listUsersReservations } from "./reservations";
 import { listResources } from "../resources/resources";
 import { Link } from "react-router-dom";
@@ -27,9 +27,25 @@ export default class MyReservationsPage extends React.Component {
 
   render() {
     return (
+      <TaxonomyContext>
+        {taxonomy => {
+          return(
       <>
         {this.state.reservations !== [] ? (
-          <Table>
+          <div style={{width: "70vw", marginLeft:"auto", marginRight:"auto", marginTop:"4vw"}}>
+          <Table  striped bordered hover variant="dark">
+
+          <thead>
+
+            <tr style={{color: "orange"}}> 
+              <th>{taxonomy.resource}</th>
+              <th>Time</th>
+              <th>Reservation status</th>
+              <th>Links</th>
+            </tr>
+
+          </thead>
+
             {this.state.reservations.map((reservation) => {
               return (
                 <tbody>
@@ -39,7 +55,7 @@ export default class MyReservationsPage extends React.Component {
                         return <td>{resource.name}</td>;
                       }
                     })}
-                    <td>{reservation.slot}</td>
+                    <td>{reservation.slot.split("T")[0] + " " + reservation.slot.split("T")[1]}</td>
                     <td>{reservation.status}</td>
                     <td>
                       <Link to={`/my-reservations/${reservation.id}`}>
@@ -51,10 +67,15 @@ export default class MyReservationsPage extends React.Component {
               );
             })}
           </Table>
+          </div>
         ) : (
           <h1>You have no reservations yet</h1>
         )}
       </>
+                )
+              }
+              }
+            </TaxonomyContext>
     );
   }
 }
