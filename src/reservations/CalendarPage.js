007e3da6542f0a656f5a4ae16ae8e7e2e5781a06
szcsl <42765413+szcsl@users.noreply.github.com>
2021-07-28 09:10:53 +0200

    refactor: improve legibility

--- src/reservations/CalendarPage.js
+++ src/reservations/CalendarPage.js
@@ -18,12 +18,20 @@ export default class CalendarPage extends React.Component {
             .then(response=>response.json())
             .then(slotConfig => listReservations()
                                   .then(reservations => reservations.filter(r => r.resource == this.props.resourceId) )
-                                  .then(filteredReservations => this.setState({reservations: filteredReservations, slotConfig: slotConfig })))
+                                  .then(filteredReservations => this.setState({
+                                    reservations: filteredReservations,
+                                    slotConfig: slotConfig
+                                  })))
   }
   
   render() {
     // amíg nincs slotConfig, nem tudjuk megjeleníteni a calendart
     if (!this.state.slotConfig) return null;
-    return <Calendar reservations={this.state.reservations} slotStart={this.state.slotConfig.start} slotEnd={this.state.slotConfig.end} slotDuration={this.state.slotConfig.duration} />
+
+    return (<Calendar
+      reservations={this.state.reservations}
+      slotStart={this.state.slotConfig.start}
+      slotEnd={this.state.slotConfig.end}
+      slotDuration={this.state.slotConfig.duration} />);
   }
 }
