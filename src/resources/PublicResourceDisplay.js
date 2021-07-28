af085400675b6438148c86f122709c8850ec5c5c
Schneider Sara <schneiders92@users.noreply.github.com>
2021-07-28 16:09:39 +0200

    sol:public resources css done

--- src/resources/PublicResourceDisplay.js
+++ src/resources/PublicResourceDisplay.js
@@ -1,39 +1,47 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {Button} from "react-bootstrap";
 
-export default class PublicResourceDisplay extends React.Component{
+export default class PublicResourceDisplay extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             hover: false
         }
     }
 
 
     render() {
-        let defeaultStyle = {backgroundImage:this.props.resource.imgUrl?`url("${this.props.resource.imgUrl}")`: "url('defaultBarber.jpg')"}
+        let defeaultStyle = {backgroundImage: this.props.resource.imgUrl ? `url("${this.props.resource.imgUrl}")` : "url('defaultBarber.jpg')"}
 
-        let hoverStyle = {opacity: "25%", filter:"blur(10px)",backgroundImage:this.props.resource.imgUrl?`url("${this.props.resource.imgUrl}")`: "url('defaultBarber.jpg')" }
+        let hoverStyle = {
+            opacity: "25%",
+            filter: "blur(10px)",
+            backgroundImage: this.props.resource.imgUrl ? `url("${this.props.resource.imgUrl}")` : "url('defaultBarber.jpg')"
+        }
 
 
         return (<div className="public-resource-display-container"
-                     onMouseOver={()=>this.setState({hover:true})}
-                     onMouseOut={()=>this.setState({hover:false})}>
-                    <div className="pub-res"
-                         style={this.state.hover ? hoverStyle : defeaultStyle}
-                        >
-                    </div>
-                        <div className="public-res-inner-container "
-                             style={{filter:"none"}}>
-                            <h5>{this.props.resource.name}</h5>
-                            <p>{this.props.resource.description}</p>
-                            <Link to={`/resources/${this.props.resource.id}/request-reservation`}>
-                                Request Reservation
-                            </Link>
-                        </div>
+                     onMouseOver={() => this.setState({hover: true})}
+                     onMouseOut={() => this.setState({hover: false})}>
+                <div className="pub-res"
+                     style={this.state.hover ? hoverStyle : defeaultStyle}
+                >
+                </div>
+                <div className="public-res-inner-container "
+                     style={{filter: "none"}}>
+                    <h2>{this.props.resource.name}</h2>
+                    <p>{this.props.resource.description}</p>
+                    <Link to={`/resources/${this.props.resource.id}/request-reservation`}>
+                        <Button className="pub-resource-btn" >
+                            Request Reservation
+                        </Button>
 
-                    </div>
+                    </Link>
+                </div>
 
-   )
+            </div>
+
+        )
     }
 }
