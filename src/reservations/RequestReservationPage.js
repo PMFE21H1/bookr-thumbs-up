b72a1d64c00a987b6abe2d898fa8ce0461b70bbe
patrik <you@example.com>
2021-07-28 14:53:46 +0200

    finish request-reservation-bootstrap

--- src/reservations/RequestReservationPage.js
+++ src/reservations/RequestReservationPage.js
@@ -1,56 +1,92 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import SlotSelector from "./SlotSelector";
-import {createReservation, listReservations, Reservation} from "./reservations";
-import {AuthContext, UsersDatabaseContext} from "../context/context"
-import {Link} from "react-router-dom";
-
+import {
+  createReservation,
+  listReservations,
+  Reservation,
+} from "./reservations";
+import { AuthContext, UsersDatabaseContext } from "../context/context";
+import { Link } from "react-router-dom";
+import { Form, Container, Row, Col, Nav, Button } from "react-bootstrap";
 export default class RequestReservationPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            reservations: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      reservations: [],
+    };
+  }
 
-    componentDidMount() {
-        listReservations()
-            .then((reservations) => this.setState({
-                    reservations: reservations
-                })
-            )
-    }
-    changeSlot=(date, time) => {
-        this.setState({
-            date: date,
-            time: time
-        })
-    }
+  componentDidMount() {
+    listReservations().then((reservations) =>
+      this.setState({
+        reservations: reservations,
+      })
+    );
+  }
+  changeSlot = (date, time) => {
+    this.setState({
+      date: date,
+      time: time,
+    });
+  };
 
-
-
-    render() {
-        return (
-            <>
-                <AuthContext.Consumer>
-                    {({ user, ...rest }) => {
-                        return (
-                <form>
-                <SlotSelector resource={ this.props.match.params.resourceID} changeSlot={this.changeSlot}/>
-                <button onClick={ (e) => {
-                    e.preventDefault()
-                    try{createReservation(new Reservation(user.uid, this.props.match.params.resourceID, `${this.state.date}T${this.state.time}`,"pending"))
-                        .catch((e) => alert(e.message))
-                        .then(()=>this.props.history.push('/my-reservations'))} catch(error){alert(error)}
-
-
-
-                }} > Create </button>
-                    <Link to ="/resources"> Cancel </Link>
-            </form>)
-                    }}
-                </AuthContext.Consumer>
-            </>
-
-        )
-    }
+  render() {
+    return (
+      <>
+        <Container>
+          <Row className="mt-5">
+            <Col
+              lg={5}
+              md={6}
+              sm={12}
+              className="p-5 m-auto shadow-sm rounded-lg "
+            >
+              <AuthContext.Consumer>
+                {({ user, ...rest }) => {
+                  return (
+                    <Form>
+                      <SlotSelector
+                        resource={this.props.match.params.resourceID}
+                        changeSlot={this.changeSlot}
+                      />
+                      <Nav className="justify-content-center">
+                        <Button
+                        style={{ marginTop: "1vw", marginRight: "1vw" }}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            try {
+                              createReservation(
+                                new Reservation(
+                                  user.uid,
+                                  this.props.match.params.resourceID,
+                                  `${this.state.date}T${this.state.time}`,
+                                  "pending"
+                                )
+                              )
+                                .catch((e) => alert(e.message))
+                                .then(() =>
+                                  this.props.history.push("/my-reservations")
+                                );
+                            } catch (error) {
+                              alert(error);
+                            }
+                          }}
+                        >
+                          {" "}
+                          Create{" "}
+                        </Button>
+                        <Button style={{ marginTop: "1vw" }} className="cancel-btn" variant="danger">
+                          <Link style={{ textDecoration: "none", color: "white" }} to="/resources"> Cancel </Link>
+                        </Button>
+                      </Nav>
+                    </Form>
+                  );
+                }}
+              </AuthContext.Consumer>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
