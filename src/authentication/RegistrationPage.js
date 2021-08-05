16437366cd49856c0d0f56943a918d731b29ea08
szcsl <42765413+szcsl@users.noreply.github.com>
2021-08-05 16:41:06 +0200

    refactor list reservation page useContext

--- src/authentication/RegistrationPage.js
+++ src/authentication/RegistrationPage.js
@@ -1,43 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { saveAccountToDatabase } from "./authentication";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row, Form, Button, Image } from "react-bootstrap";
 import "./registration.css";
 
-export default class RegistrationPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      confirmation: "",
-      name: "",
-    };
-  }
+export default function RegistrationPage(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+  const [name, setName] = useState("");
 
-  register = () => {
-    if (this.state.password === this.state.confirmation) {
+  function register() {
+    if (password === confirmation) {
       const auth = getAuth();
       createUserWithEmailAndPassword(
         auth,
-        this.state.email,
-        this.state.password
+        email,
+        password
       )
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          user.name = this.state.name;
+          user.name = name;
           user.admin = false;
           console.log(user);
           saveAccountToDatabase(
-            this.state.name,
-            this.state.email,
+            name,
+            email,
             false,
             user.uid
           );
-          this.props.onLogIn(user);
-          this.props.history.push("/user/my-reservations");
+          props.onLogIn(user);
+          props.history.push("/user/my-reservations");
         })
 
         .catch((error) => {
@@ -50,85 +45,61 @@ export default class RegistrationPage extends Component {
         "The password and the confirmation-password didn't match"
       );
     }
-  };
-
-  handleEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
-  handlePassword = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
-  };
-  handleConfirmation = (e) => {
-    this.setState({
-      confirmation: e.target.value,
-    });
-  };
-  handleName = (e) => {
-    this.setState({
-      name: e.target.value,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Container className="registration-super-container">
-          <Row className="mt-4">
-            <Col
-              lg={5}
-              md={6}
-              sm={12}
-              className="p-5 m-auto shadow-sm rounded-lg "
-            >
-              <div className="registration-cont  p-5 ">
-                <Form.Group>
-                  <Form.Label>Customer Name</Form.Label>
-                  <Form.Control
-                    placeholder="Enter your name"
-                    onChange={(e) => this.handleName(e)}
-                  ></Form.Control>
-                  <Form.Label className="mt-2">Email Address</Form.Label>
-                  <Form.Control
-                    placeholder="Enter your email"
-                    type="email"
-                    onChange={(e) => this.handleEmail(e)}
-                  ></Form.Control>
-                  <Form.Label className="mt-2">Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter a new password"
-                    onChange={(e) => this.handlePassword(e)}
-                  ></Form.Control>
-                  <Form.Label className="mt-2">
-                    Password Confirmation
-                  </Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter new password again"
-                    onChange={(e) => this.handleConfirmation(e)}
-                  ></Form.Control>
-                  <Button
-                    size="lg"
-                    className="mt-4 reg-btn"
-                    variant="info btn-block"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      this.register();
-                    }}
-                  >
-                    Registration
-                  </Button>
-                </Form.Group>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
   }
-}
-{
+
+  return (
+    <div>
+      <Container className="registration-super-container">
+        <Row className="mt-4">
+          <Col
+            lg={5}
+            md={6}
+            sm={12}
+            className="p-5 m-auto shadow-sm rounded-lg "
+          >
+            <div className="registration-cont  p-5 ">
+              <Form.Group>
+                <Form.Label>Customer Name</Form.Label>
+                <Form.Control
+                  placeholder="Enter your name"
+                  onChange={e => setName(e.target.value)}
+                ></Form.Control>
+                <Form.Label className="mt-2">Email Address</Form.Label>
+                <Form.Control
+                  placeholder="Enter your email"
+                  type="email"
+                  onChange={e => setEmail(e.target.value)}
+                ></Form.Control>
+                <Form.Label className="mt-2">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter a new password"
+                  onChange={e => setPassword(e.target.value)}
+                ></Form.Control>
+                <Form.Label className="mt-2">
+                  Password Confirmation
+                </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter new password again"
+                  onChange={e => setConfirmation(e.target.value)}
+                ></Form.Control>
+                <Button
+                  size="lg"
+                  className="mt-4 reg-btn"
+                  variant="info btn-block"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    register();
+                  }}
+                >
+                  Registration
+                </Button>
+              </Form.Group>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
--- src/reservations/listReservationsPage.js
+++ src/reservations/listReservationsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Link } from "react-router-dom";
 import {
   AuthContext,
@@ -10,8 +10,182 @@ import { listReservations, confirmReservation } from "./reservations";
 import Table from "react-bootstrap/Table";
 import { Button, Nav } from "react-bootstrap";
 
+export default function ListReservationsPage() {
+  const taxonomy = useContext(TaxonomyContext);
+  const users = useContext(UsersDatabaseContext);
+  return (
 
-export default class ListReservationsPage extends React.Component {
+      
+                  <>
+                    <Nav>
+                      <Nav.Item>
+                        <Nav.Link as={Link} to={"/admin/reservations/create"}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-columns"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V2zm8.5 0v8H15V2H8.5zm0 9v3H15v-3H8.5zm-1-9H1v3h6.5V2zM1 14h6.5V6H1v8z" />
+                          </svg>{" "}
+                          Add reservation
+                        </Nav.Link>
+                      </Nav.Item>
+
+                    </Nav>
+
+                    <h2>Confirmed Reservations</h2>
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Reservations customer</th>
+                          <th>Reservations id</th>
+                          <th>Reservations time</th>
+                          <th>{taxonomy.resource}</th>
+                        </tr>
+                      </thead>
+
+                      {this.state.confirmed.map((reservation) => {
+                        return (
+                          <tbody>
+                            <tr>
+                              <td>
+                                {users.map((user) =>
+                                  reservation.customerUid === user.uid
+                                    ? user.name
+                                    : ""
+                                )}
+                              </td>
+                              <td>{reservation.id}</td>
+                              <td>{reservation.slot.split("T")[0] + " " + reservation.slot.split("T")[1]}</td>
+                              <td>
+                                {this.state.resources.map((resource) =>
+                                  reservation.resource == resource.id
+                                    ? resource.name
+                                    : ""
+                                )}
+                              </td>
+                              <td>
+                                <Nav.Item>
+                                  <Nav.Link
+                                    as={Link}
+                                    to={`/admin/reservations/${reservation.id}/delete`}
+                                  >
+                                    DELETE
+                                  </Nav.Link>
+                                </Nav.Item>
+                              </td>
+                              <td>
+                                <Nav.Item>
+                                  <Nav.Link
+                                    as={Link}
+                                    to={`/admin/reservations/${reservation.id}/edit`}
+                                  >
+                                    EDIT
+                                  </Nav.Link>
+                                </Nav.Item>
+                              </td>
+                              <td>
+                                <Nav.Item>
+                                  <Nav.Link
+                                    as={Link}
+                                    to={`/admin/reservations/${reservation.id}`}
+                                  >
+                                    DETAILS
+                                  </Nav.Link>
+                                </Nav.Item>
+                              </td>
+                            </tr>
+                          </tbody>
+                        );
+                      })}
+                    </Table>
+
+                    <h2>Pending Reservations</h2>
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Reservations customer</th>
+                          <th>Reservations id</th>
+                          <th>Reservations time</th>
+                          <th>{taxonomy.resource}</th>
+                        </tr>
+                      </thead>
+
+                      {this.state.pending.map((reservation) => {
+                        return (
+                          <tbody>
+                            <tr>
+                              <td>
+                                {users.map((user) =>
+                                  reservation.customerUid === user.uid
+                                    ? user.name
+                                    : ""
+                                )}{" "}
+                              </td>
+                              <td>{reservation.id}</td>
+                              <td>{reservation.slot.split("T")[0] + " " + reservation.slot.split("T")[1]}</td>
+                              <td>
+                                {this.state.resources.map((resource) =>
+                                  reservation.resource == resource.id
+                                    ? resource.name
+                                    : ""
+                                )}
+                              </td>
+                              <td>
+                                <Nav.Item>
+                                  <Nav.Link
+                                    as={Link}
+                                    to={`/admin/reservations/${reservation.id}/delete`}
+                                  >
+                                    DELETE
+                                  </Nav.Link>
+                                </Nav.Item>
+                              </td>
+                              <td>
+                                <Nav.Item>
+                                  <Nav.Link
+                                    as={Link}
+                                    to={`/admin/reservations/${reservation.id}/edit`}
+                                  >
+                                    EDIT
+                                  </Nav.Link>
+                                </Nav.Item>
+                              </td>
+                              <td>
+                                <Nav.Item>
+                                  <Nav.Link
+                                    as={Link}
+                                    to={`/admin/reservations/${reservation.id}`}
+                                  >
+                                    DETAILS
+                                  </Nav.Link>
+                                </Nav.Item>
+                              </td>
+                              <td>
+                                <Button
+                                  onClick={() =>
+                                    {
+                                    confirmReservation(reservation.id)
+                                    .then(window.location.reload())
+                                  }
+                                  }
+                                >
+                                  CONFIRM
+                                </Button>
+                              </td>
+                            </tr>
+                          </tbody>
+                        );
+                      })}
+                    </Table>
+                  </>
+    );
+}
+
+class OldListReservationsPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
